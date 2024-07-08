5c312aef5294b37bbd09d775a7f0cdcefaab3c68
Danialdevs <107905839+Danialdevs@users.noreply.github.com>
2024-07-08 16:28:30 +0500

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,25 @@
 <template>
   <div>
-      <span class="font-bold text-2xl">привет я текст</span>
-    <Button>Ку</Button>
+    <div>
+      <MyHeader />
+    </div>
+
+    <div class="flex-auto ">
+      <div class="container mx-auto px-4 py-8 ">
+        <h1 class="text-2xl font-bold mb-4">Your Content Here</h1>
+        <p v-for="key in 70">This is your main content area. You can add more components and content here.</p>
+
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script setup lang="ts">
-import { Button } from '@/components/ui/button';
+
+import MyHeader from '@/components/My-Header.vue';
 </script>
 
-<style scoped></style>
+<style scoped>
+/* Add scoped styles for your content block if needed */
+</style>
---
+++ src/components/My-Header.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+
+import {
+  DropdownMenu, DropdownMenuContent,
+  DropdownMenuItem, DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from '@/components/ui/navigation-menu';
+import { CircleUser, Package } from 'lucide-vue-next';
+</script>
+
+<template>
+  <header class="fixed bottom-0 left-0 right-0 md:sticky md:top-0 flex h-16 items-center gap-4 border-t md:border-b align-center bg-background px-4 md:px-6">
+    <nav class="flex w-full items-center justify-center gap-6 text-lg font-medium">
+      <NavigationMenu>
+        <Package class="mr-5 hidden md:flex" />
+        <NavigationMenuList class="flex gap-5 my-5 md:my-0">
+          <NavigationMenuItem class="flex flex-col items-center justify-center h-full">
+            <Package class="md:hidden" />
+            <NavigationMenuLink>Главная</NavigationMenuLink>
+          </NavigationMenuItem>
+
+          <NavigationMenuItem class="flex flex-col items-center justify-center h-full">
+            <CircleUser class="md:hidden" />
+            <NavigationMenuLink>Профиль</NavigationMenuLink>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+      <div class="ml-auto relative hidden md:flex">
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <CircleUser />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Settings</DropdownMenuItem>
+            <DropdownMenuItem>Support</DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Logout</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+
+</style>
