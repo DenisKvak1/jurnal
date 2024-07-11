d0f41d613cf92fb72a275abbad989c99fe7967cd
DenisKvak1 <59225772+DenisKvak1@users.noreply.github.com>
2024-07-11 09:29:49 +0300

    Update

--- src/components/TimeTable.vue
+++ src/components/TimeTable.vue
@@ -1,60 +1,97 @@
 <script setup lang="ts">
 import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
+    Carousel,
+    CarouselContent,
+    CarouselItem,
 } from '@/components/ui/carousel';
 import { DiaryDayElement } from '@/types.ts';
-import { getDayOfWeek } from '@internationalized/date';
 import { useRegionStore } from '@/store/RegionStore.ts';
 import { getDayOfWeekString } from '../helpers/formatTime.ts';
 import { computed } from 'vue';
+import { useWindowSize } from '@vueuse/core';
+import { Table, TableBody, TableCell, TableHeader, TableRow } from '@/components/ui/table';
 
-const regionStore = useRegionStore()
+const { width } = useWindowSize();
+const regionStore = useRegionStore();
 const props = defineProps({
-  timeTable: {
-    required: true,
-    type: Array as () => DiaryDayElement[],
-  },
+    timeTable: {
+        required: true,
+        type: Array as () => DiaryDayElement[],
+    },
 });
+
+function findLongestSubjectsArray(objects: DiaryDayElement[]): any[] {
+    return objects.reduce((longest, current) => {
+        return current.subjects.length > longest.length ? current.subjects : longest;
+    }, [] as any[]);
+}
+
 
 </script>
 
 <template>
-  <div>
-    <Carousel>
-      <CarouselContent>
-        <CarouselItem v-for="dayElement in props.timeTable">
-          <div class="flex flex-col gap-1">
-            <h4 class="scroll-m-20 text-xl font-semibold tracking-tight">
-                {{ computed(() => getDayOfWeekString(dayElement.date, regionStore.language)) }}
-            </h4>
-            <div class="flex flex-col last-of-type:border-b">
-              <div
-                class="flex items-center border-t p-3 border-l border-r"
-                v-for="(subject, index) in dayElement.subjects"
-              >
-                <div class="w-[30px]">
-                  {{ index + 1 }}
-                </div>
-                <div class="w-[60px] flex flex-col gap-1">
-                  <span class="font-medium">{{subject.callStart}}</span>
-                  <span class="font-medium">{{subject.callEnd}}</span>
-                </div>
-                <div class="flex flex-col gap-1">
-                  <span class="font-medium text-lg"> {{ subject.name }} </span>
-                  <span class="text-sm text-[#6b4cff] dark:text-[#646464]">
-                    {{subject.teacher}}
+    <div>
+        <Carousel v-if="width < 1024">
+            <CarouselContent>
+                <CarouselItem v-for="dayElement in props.timeTable">
+                    <div class="flex flex-col gap-1">
+                        <h4 class="scroll-m-20 text-xl font-semibold tracking-tight">
+                            {{ computed(() => getDayOfWeekString(dayElement.date, regionStore.language)) }}
+                        </h4>
+                        <div class="flex flex-col last-of-type:border-b">
+                            <div
+                                class="flex items-center border-t p-3 border-l border-r"
+                                v-for="(subject, index) in dayElement.subjects"
+                            >
+                                <div class="w-[30px]">
+                                    {{ index + 1 }}
+                                </div>
+                                <div class="w-[60px] flex flex-col gap-1">
+                                    <span class="font-medium">{{ subject.callStart }}</span>
+                                    <span class="font-medium">{{ subject.callEnd }}</span>
+                                </div>
+                                <div class="flex flex-col gap-1">
+                                    <span class="font-medium text-lg"> {{ subject.name }} </span>
+                                    <span class="text-sm text-[#6b4cff] dark:text-[#646464]">
+                    {{ subject.teacher }}
                   </span>
-                  <span class="text-sm"> {{ subject.cabinet}} </span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </CarouselItem>
-      </CarouselContent>
-    </Carousel>
-  </div>
+                                    <span class="text-sm"> {{ subject.cabinet }} </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </CarouselItem>
+            </CarouselContent>
+        </Carousel>
+        <Table v-if="width >= 1024">
+            <TableHeader>
+
+            </TableHeader>
+            <TableBody>
+                <TableRow v-for="(day, dayIndex) in findLongestSubjectsArray(props.timeTable)">
+                    <TableCell class="w-[120px] lg:w-[160px]">
+                        <div class="flex justify-between">
+                            <div class="flex flex-col">
+                                <span class="text-base font-medium">{{ day.callStart }}</span>
+                                <span class="text-base font-medium">{{ day.callEnd }}</span>
+                            </div>
+                            <span class="text-base font-medium">
+                                {{ dayIndex + 1 }}
+                            </span>
+                        </div>
+                    </TableCell>
+                    <TableCell v-for="index in 7" class="w-[200px]">
+                        <div class="flex flex-col gap-1">
+                            <span class="text-base"> {{ props.timeTable[dayIndex]?.subjects[index]?.name }} </span>
+                            <span
+                                class="text-blue-600"> {{ props.timeTable[dayIndex]?.subjects[index]?.teacher }}</span>
+                            <span> {{ props.timeTable[dayIndex]?.subjects[index]?.cabinet }}</span>
+                        </div>
+                    </TableCell>
+                </TableRow>
+            </TableBody>
+        </Table>
+    </div>
 </template>
 
 <style scoped></style>
--- src/page/MainPage.vue
+++ src/page/MainPage.vue
@@ -60,6 +60,96 @@ const days = ref([
         date: '01.06.24',
         subjects: [
             {
+                id: 5,
+                lessonId: 2,
+                callStart: '8:45',
+                callEnd: '9:45',
+                name: '1 урока',
+                cabinet: '3',
+                teacher: 'Клавида петровна2',
+                current: '4',
+                homeWork: '5',
+                mark: 9,
+            },
+            {
+                id: 45,
+                lessonId: 3,
+                callStart: '8:45',
+                callEnd: '78:45',
+                name: '31321dsa',
+                cabinet: 'Кабінеxzxxcz ії[36]',
+                current: '44',
+                homeWork: '44',
+                teacher: 'Клавида петровна4',
+                mark: 6,
+            },
+            {
+                id: 1,
+                lessonId: 4,
+                callStart: '8:45',
+                callEnd: '9:45',
+                name: '31321dsa',
+                cabinet: 'Кабінет географії[36]',
+                current: '5',
+                teacher: 'Клавида петровна6',
+                homeWork: '6',
+                mark: 10,
+            },
+            {
+                id: 1,
+                lessonId: 4,
+                callStart: '8:45',
+                callEnd: '9:45',
+                name: '31321dsa',
+                cabinet: 'Кабінет географії[36]',
+                current: '5',
+                teacher: 'Клавида петровна6',
+                homeWork: '6',
+                mark: 10,
+            },
+            {
+                id: 1,
+                lessonId: 4,
+                callStart: '8:45',
+                callEnd: '9:45',
+                name: '31321dsa',
+                cabinet: 'Кабінет географії[36]',
+                current: '5',
+                teacher: 'Клавида петровна6',
+                homeWork: '6',
+                mark: 10,
+            },
+            {
+                id: 1,
+                lessonId: 4,
+                callStart: '8:45',
+                callEnd: '9:45',
+                name: '31321dsa',
+                cabinet: 'Кабінет географії[36]',
+                current: '5',
+                teacher: 'Клавида петровна6',
+                homeWork: '6',
+                mark: 10,
+            },
+            {
+                id: 1,
+                lessonId: 4,
+                callStart: '8:45',
+                callEnd: '9:45',
+                name: '31321dsa',
+                cabinet: 'Кабінет географії[36]',
+                current: '5',
+                teacher: 'Клавида петровна6',
+                homeWork: '6',
+                mark: 10,
+            },
+        ],
+    },
+    {
+        id: 2,
+        date: '02.06.24',
+        subjects: [
+            {
                 id: 123,
                 lessonId: 1,
                 callStart: '8:45',
@@ -107,61 +197,43 @@ const days = ref([
                 homeWork: '6',
                 mark: 10,
             },
-        ]
-    },
-    {
-        id: 2,
-        date: '02.06.24',
-        subjects: [
             {
-                id: 123,
-                lessonId: 1,
+                id: 1,
+                lessonId: 4,
                 callStart: '8:45',
                 callEnd: '9:45',
-                name: 'Урок урока',
-                cabinet: 'Кабинет урока уроковича',
-                current: 'Изучение урока',
-                teacher: 'Клавида петровна',
-                homeWork: 'Домашука урока уроковича',
-                mark: 2,
-            },
-            {
-                id: 5,
-                lessonId: 2,
-                callStart: '8:45',
-                callEnd: '9:45',
-                name: '212qwdadwa',
-                teacher: 'Клавида петровна',
-                cabinet: '3',
-                current: '4',
-                homeWork: '5',
-                mark: 9,
-            },
-            {
-                id: 45,
-                lessonId: 3,
-                callStart: '8:45',
-                callEnd: '78:45',
                 name: '31321dsa',
-                cabinet: 'Кабінеxzxxcz ії[36]',
-                teacher: 'Клавида петровна',
-                current: '44',
-                homeWork: '44',
-                mark: 6,
+                cabinet: 'Кабінет географії[36]',
+                current: '5',
+                teacher: 'Клавида петровна6',
+                homeWork: '6',
+                mark: 10,
             },
             {
                 id: 1,
                 lessonId: 4,
                 callStart: '8:45',
                 callEnd: '9:45',
-                teacher: 'Клавида петровна',
                 name: '31321dsa',
                 cabinet: 'Кабінет географії[36]',
                 current: '5',
+                teacher: 'Клавида петровна6',
                 homeWork: '6',
                 mark: 10,
             },
-        ]
+            {
+                id: 1,
+                lessonId: 4,
+                callStart: '8:45',
+                callEnd: '9:45',
+                name: '31321dsa',
+                cabinet: 'Кабінет географії[36]',
+                current: '5',
+                teacher: 'Клавида петровна6',
+                homeWork: '6',
+                mark: 10,
+            },
+        ],
     },
     {
         id: 3,
@@ -204,9 +276,21 @@ const days = ref([
                 mark: 6,
             },
             {
+                id: 123,
+                lessonId: 4,
+                callStart: '8:45',
+                callEnd: '9:45',
+                name: 'Урок урока',
+                cabinet: 'Кабинет урока уроковича',
+                current: 'Изучение урока',
+                homeWork: 'Домашука урока уроковича',
+                teacher: 'Клавида петровна',
+                mark: 2,
+            },
+            {
                 teacher: 'Клавида петровна',
                 id: 1,
-                lessonId: 4,
+                lessonId: 5,
                 callStart: '8:45',
                 callEnd: '9:45',
                 name: '31321dsa',
@@ -215,8 +299,8 @@ const days = ref([
                 homeWork: '6',
                 mark: 10,
             },
-        ]
-    }
+        ],
+    },
 ]);
 
 const marks = ref([
@@ -243,15 +327,15 @@ const marks = ref([
     <div class="px-4">
         <TimeTable :timeTable="days"></TimeTable>
     </div>
-<!--    <div class="lg:w-[680px]">-->
-<!--        <DiaryElement :date="'24-07-08T16:17:12+00:00'" :lessons="les" />-->
-<!--        <set-marks-->
-<!--            class="mt-5"-->
-<!--            :subject-name="'Математика'"-->
-<!--            :marks="marks as any"-->
-<!--            :week-date="{ startWeekTIMESTAMP: '24-07-09T08:40:43+00:00' }"-->
-<!--        ></set-marks>-->
-<!--    </div>-->
+    <!--    <div class="lg:w-[680px]">-->
+    <!--        <DiaryElement :date="'24-07-08T16:17:12+00:00'" :lessons="les" />-->
+    <!--        <set-marks-->
+    <!--            class="mt-5"-->
+    <!--            :subject-name="'Математика'"-->
+    <!--            :marks="marks as any"-->
+    <!--            :week-date="{ startWeekTIMESTAMP: '24-07-09T08:40:43+00:00' }"-->
+    <!--        ></set-marks>-->
+    <!--    </div>-->
 </template>
 
 <style scoped></style>
